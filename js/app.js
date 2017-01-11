1ccbd780f53b6852b39d897d2b46b1c213d2aada
Corneliousm808 <corneliousmooring@gmail.com>
2017-01-11 08:27:50 -1000

    fortune

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 function corn() {
   console.log(im bored);
   var changeDiv = document.getElementById('divElement');
-  changeDiv.inner.HTML = "hi ";
-  console.log()
+  changeDiv.inner.HTML = "hi";
+  console.log(changeDiv);
+}
+
+function fortune generator() {
+  var fortuneList = ['good stuff coming', 'you will be able to breathe tomorrow', 'you will become invisible', 'someone will tell you to shut up', 'you wiil see']
+
+  var random = fortunesList[Math.floor(Math.random() * fortunesList.length)];
+  document.getElementById('text').textContent = random;
+
+  console.log('User ' + random)
+
+  var list = document.getElementById('someting');
+  var node = document.createElement("div");
+  node.innerHTML = random
+  list.appendChild(node);
 }
