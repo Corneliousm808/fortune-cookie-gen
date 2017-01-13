6f325adb1b8d148525fd86a84f421dc5d3e43979
Corneliousm808 <corneliousmooring@gmail.com>
2017-01-13 09:16:06 -1000

    its not linked idk why

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
-function corn() {
-  console.log(im bored);
-  var changeDiv = document.getElementById('divElement');
-  changeDiv.inner.HTML = "hi";
-  console.log(changeDiv);
-}
 
-function fortune generator() {
-  var fortuneList = ['good stuff coming', 'you will be able to breathe tomorrow', 'you will become invisible', 'someone will tell you to shut up', 'you wiil see']
+var fortunesToSay = ['good stuff coming', 'you will be able to breathe tomorrow', 'you will die between now and 50 years', 'someone will tell you to shut up', 'you wiil see'];
+var fortune="";
+var saidAll=false;
 
-  var random = fortunesList[Math.floor(Math.random() * fortunesList.length)];
-  document.getElementById('text').textContent = random;
+function generateFortuneCookie() {
+  if(saidAll==false){
+    var btn= document.getElementById('onlyButton')
+    var box = document.getElementById('fortune-cookie-text');
+    var list = document.getElementById('list');
+    var li = document.createElement("li");
+    btn.innerHTML="Hit me baby one more time!";
 
-  console.log('User ' + random)
+    if (fortune!=""){
+      li.appendChild(document.createTextNode(fortune));
+      list.appendChild(li);
+    }
+    if (fortunesToSay.length>0) {
+      var num;
+      num =Math.floor(Math.random()*fortunesToSay.length);
+      fortune = fortunesToSay[num];
 
-  var list = document.getElementById('someting');
-  var node = document.createElement("div");
-  node.innerHTML = random
-  list.appendChild(node);
+      fortunesToSay.splice(num, 1);
+      box.innerHTML = "<b>Fortunes</b> <br><br>"+fortune;
+    }
+    else{
+      btn.innerHTML="You ran out of fortunes!";
+      box.innerHTML = "<b>Fortunes</b> <br><br> Nomore, get away!";
+      saidAll=true;
+    }
+  }
 }
